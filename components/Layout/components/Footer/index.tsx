fca65fad0159850034375c60ceee9563661ab1b7
svitlana <swetamazkiw@gmail.com>
2021-12-30 04:49:18 +0200

    feat: add planets list on homepage

--- components/Layout/components/Footer/index.tsx
+++ components/Layout/components/Footer/index.tsx
@@ -4,7 +4,7 @@ const Footer = () => (
   <footer className="flex-shrink-0 h-16 w-full flex justify-center items-center">
     Made with ❤️ by&nbsp;
     <Link href="https://www.linkedin.com/in/svitlana-matskiv">
-      Svitlana Matskiv
+      <span className="underline">Svitlana Matskiv</span>
     </Link>
   </footer>
 )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,46 @@
-import type { NextPage } from 'next'
+import type { NextPage, NextPageContext } from 'next'
+import Link from 'next/link'
+import PlanetCard from '@/components/PlanetCard'
+import { BasicPlanet } from '@/lib/models/planet'
+import { getAllPlanets } from '@/lib/services/planet.service'
 
-const Home: NextPage = () => {
+interface IProps extends NextPageContext {
+  planets: BasicPlanet[]
+}
+
+const HomePage: NextPage<IProps> = ({ planets }) => {
   return (
-    <>
-      <h1 className="text-center my-24 font-black tracking-tight text-6xl">
-        Homepage
+    <div className="__app-container ">
+      <h1
+        className="text-center mb-8 font-black tracking-tight 
+        text-3xl xl:text-5xl 2xl:text-6xl"
+      >
+        Planets
       </h1>
-    </>
+      <div
+        className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 
+        xl:grid-cols-5 sm:gap-x-5 lg:gap-x-10 gap-y-6 lg:gap-y-10"
+      >
+        {planets.map((x) => (
+          <Link href={`/planet/${x.id}`} key={x.name + x.id} passHref>
+            <a role="link" aria-label={`${x.name} Planet`}>
+              <PlanetCard planet={x} />
+            </a>
+          </Link>
+        ))}
+      </div>
+    </div>
   )
 }
 
-export default Home
+export default HomePage
+
+export async function getStaticProps() {
+  const planets: BasicPlanet[] = await getAllPlanets()
+
+  return {
+    props: {
+      planets,
+    },
+  }
+}
